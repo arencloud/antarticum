2532dd2db00c87362ff10114728178f86469a8ad
dreamofdoc <qamanchajyan.2002@gmail.com>
2023-10-16 20:57:09 +0400

    added middleware function

---
+++ internal/middlewares/data_format.go
@@ -0,0 +1,62 @@
+package middlewares
+
+import (
+	"encoding/xml"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
+	"net/http"
+	"strings"
+)
+
+func FormatMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		format := strings.ToLower(c.DefaultQuery("format", "json"))
+		switch format {
+		case "json":
+			c.Request.Header.Set("Accept", "application/json")
+		case "xml":
+			c.Request.Header.Set("Accept", "application/xml")
+		case "yaml":
+			c.Request.Header.Set("Accept", "application/yaml")
+		default:
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Unsupported format"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+
+		switch format {
+		case "json":
+			// Do nothing, response is already in JSON format
+		case "xml":
+			var data CommonDataStructure
+			if err := c.ShouldBindJSON(&data); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			xmlData, err := xml.Marshal(data)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			c.Data(http.StatusOK, "application/xml", xmlData)
+		case "yaml":
+			var data CommonDataStructure
+			if err := c.ShouldBindJSON(&data); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			yamlData, err := yaml.Marshal(data)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			c.Data(http.StatusOK, "application/yaml", yamlData)
+		}
+	}
+}
